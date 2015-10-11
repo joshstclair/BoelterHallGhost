2852c6571e3af6cc6f0f925fc78e1284f84266e3
Matt Chan <matthew.a.chan@gmail.com>
2015-10-10 18:13:36 -0700

    Add creature

--- Assets/Scripts/OpenableDoor.cs
+++ Assets/Scripts/OpenableDoor.cs
@@ -4,8 +4,8 @@ using System.Collections;
 public class OpenableDoor : MonoBehaviour {
 	public int doorOpenAngle = -90;
 	public bool playerEnter, doorOpen;
-	public float openSpeed = 2;
-	public Vector3 closedRotation, openRotation;
+	public float openSpeed = 1;
+	public Vector3 closedRotation, creakRotation, openRotation;
 
 	void Start () {
 		// Obtains the default x, y, z rotational values for a closed door
@@ -18,8 +18,10 @@ public class OpenableDoor : MonoBehaviour {
 	void Update () {
 		// If the door flag is true, open the door
 		if (doorOpen) {
+			// slightly open door
 			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, openRotation, Time.deltaTime * openSpeed);
-		} 
+			// fast open
+		}
 		// If the door flag is false, close the door
 		else {
 			transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, closedRotation, Time.deltaTime * openSpeed);
