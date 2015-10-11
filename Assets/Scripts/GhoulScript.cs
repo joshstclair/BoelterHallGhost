f9b113c091aa148c33db1e9a0c8bc85a3d76e7e7
Andrew Kim <adkim5494@gmail.com>
2015-10-10 21:23:40 -0700

    sound

--- Assets/Scripts/GhoulScript.cs
+++ Assets/Scripts/GhoulScript.cs
@@ -1,10 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Audio;
 
 public class GhoulScript : MonoBehaviour {
-	
+
+	private AudioSource source;
+
+	private AudioClip monsterScare;
+
 	void Start () {
 		//grab the camera to know the position of x and y and z
+		source = GetComponent<AudioSource> ();
 	}
 	
 	void Update() {
@@ -14,5 +20,7 @@ public class GhoulScript : MonoBehaviour {
 		Vector3 temp= Camera.main.transform.position + Camera.main.transform.forward * 2;
 		Vector3 temp2 = new Vector3 (temp.x, temp.y - 2, temp.z);
 		transform.position = temp2;
+
+
 	}
 }
